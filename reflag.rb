7b7b67e303c3754b934b02a7ec3020ac99abf6c4
= <=>
2026-01-13 22:37:45 +0100

    Fix formula to use correct binaries for both architectures

--- reflag.rb
+++ reflag.rb
@@ -6,13 +6,13 @@ class Reflag < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/kluzzebass/reflag/releases/download/v0.8.3/reflag-aarch64-apple-darwin"
-      sha256 "d8f994ca52529cbde8cda9698c9b6047e5f94c4f547b91eb1209432b4e53c11a"
+      url "https://github.com/kluzzebass/reflag/releases/download/v0.8.3/reflag-x86_64-apple-darwin"
+      sha256 "f7dbb62fb982c2d4d4738bb8b5a64955090180a72a2808d764017eae711a1eaf"
     end
 
     on_arm do
-      url "https://github.com/kluzzebass/reflag/releases/download/v0.8.2/reflag-aarch64-apple-darwin"
-      sha256 "b67f0ebe7557bc383ed3a179ebf54864ccb57e2638124424095ba5eb058ffa1f"
+      url "https://github.com/kluzzebass/reflag/releases/download/v0.8.3/reflag-aarch64-apple-darwin"
+      sha256 "d8f994ca52529cbde8cda9698c9b6047e5f94c4f547b91eb1209432b4e53c11a"
     end
   end
 
